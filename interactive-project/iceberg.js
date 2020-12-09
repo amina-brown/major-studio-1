cd29dd645151c31e79038a0d8e89f56e4c2fcce3
Amina Brown <67931920+amina-brown@users.noreply.github.com>
2020-12-09 18:13:27 -0500

    Add files via upload

--- interactive-project/iceberg.js
+++ interactive-project/iceberg.js
@@ -26,11 +26,13 @@ d3.json("institution_data.json", function(data) {
 
     // console.log(percents);
     svg.select("#top").attr("height",`${ratio * 200}%`)
+    svg.select("#top").attr("height",`204.17px`)
 
     svg.select("bottom")
     // svg.append("rect")
     // .attr("id","bottom")
-    .attr("y",`${ratio * 200}%`)
+    // .attr("y",`${ratio * 200}%`)
+    .attr("y",`204.17px`)
 
     // var surface = ratio*100;
     var surface = 204.17;
